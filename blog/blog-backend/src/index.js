d2fd429449293aa632c80b1393dfa8a028477617
forourhappy <foroh1504@gmail.com>
2022-11-24 20:51:58 +0900

    blog mongoDB 연결

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,8 +1,12 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+import { config } from 'dotenv';
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import { _MongoClient } from 'mongodb';
 
-const api = require('./api');
+import api from './api';
+
+const { PORT, url } = process.env;
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +20,15 @@ app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-	console.log('Listening to port 4000');
-});
+_MongoClient
+	.connect(url)
+	.then(client => {
+		console.log('mongo connected');
+		console.log(client);
+	})
+	.then(
+		app.listen(PORT, () => {
+			console.log('4000 port on');
+		})
+	)
+	.catch(err => console.log(err));
