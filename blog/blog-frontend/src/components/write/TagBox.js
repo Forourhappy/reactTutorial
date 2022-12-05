1ee397785fecc104dc20a49813e20b1bb8c0286e
Forourhappy <foroh1504@gmail.com>
2022-12-05 19:54:50 +0900

    blog Editor Form 작성

---
+++ blog/blog-frontend/src/components/write/TagBox.js
@@ -0,0 +1,128 @@
+import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const TagBoxWrapper = styled.div`
+	width: 100%;
+	border-top: 1px solid ${palette.gray[2]};
+	padding-top: 2rem;
+
+	h4 {
+		color: ${palette.gray[8]};
+		margin-top: 0;
+		margin-bottom: 0.5rem;
+	}
+`;
+
+const TagForm = styled.form`
+	border-radius: 4px;
+	overflow: hidden;
+	display: flex;
+	width: 256px;
+	border: 1px solid ${palette.gray[9]};
+
+	input,
+	button {
+		outline: none;
+		border: none;
+		font-size: 1rem;
+	}
+
+	input {
+		padding: 0.5rem;
+		flex: 1;
+		min-width: 0;
+	}
+
+	button {
+		cursor: pointer;
+		padding-right: 1rem;
+		padding-left: 1rem;
+		border: none;
+		background-color: ${palette.gray[8]};
+		color: white;
+		font-weight: bold;
+		&:hover {
+			background-color: ${palette.gray[6]};
+		}
+	}
+`;
+
+const Tag = styled.div`
+	margin-right: 0.5rem;
+	color: ${palette.gray[6]};
+	cursor: pointer;
+	&:hover {
+		opacity: 0.5;
+	}
+`;
+
+const TagListWrapper = styled.div`
+	display: flex;
+	margin-top: 0.5rem;
+`;
+
+// React.memo를 사용하여 tag 값이 바뀔 때만 리렌더링되도록 처리
+const TagItem = React.memo(({ tag, onRemove }) => (
+	<Tag onClick={() => onRemove(tag)}>#{tag}</Tag>
+));
+
+// React.memo를 사용하여 tags 값이 바뀔 때만 리렌더링되도록 처리
+const TagList = React.memo(({ tags, onRemove }) => (
+	<TagListWrapper>
+		{tags.map(tag => (
+			<TagItem key={tag} tag={tag} onRemove={onRemove} />
+		))}
+	</TagListWrapper>
+));
+
+const TagBox = () => {
+	const [input, setInput] = useState('');
+	const [localTags, setLocalTags] = useState([]);
+
+	const insertTag = useCallback(
+		tag => {
+			if (!tag) return;
+			if (localTags.includes(tag)) return;
+			setLocalTags([...localTags, tag]);
+		},
+		[localTags]
+	);
+
+	const onRemove = useCallback(
+		tag => {
+			setLocalTags(localTags.filter(t => t !== tag));
+		},
+		[localTags]
+	);
+
+	const onChange = useCallback(e => {
+		setInput(e.target.value);
+	}, []);
+
+	const onSubmit = useCallback(
+		e => {
+			e.preventDefault();
+			insertTag(input.trim());
+			setInput('');
+		},
+		[insertTag, input]
+	);
+
+	return (
+		<TagBoxWrapper>
+			<h4>태그</h4>
+			<TagForm onSubmit={onSubmit}>
+				<input
+					placeholder='태그를 입력하세요'
+					value={input}
+					onChange={onChange}
+				/>
+				<button type='submit'>추가</button>
+			</TagForm>
+			<TagList tags={localTags} onRemove={onRemove} />
+		</TagBoxWrapper>
+	);
+};
+
+export default TagBox;
---
+++ blog/blog-frontend/src/components/write/WriteActionButtons.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import Button from '../common/Button';
+
+const WriteActionButtonsWrapper = styled.div`
+	margin-top: 1rem;
+	margin-bottom: 3rem;
+	button + button {
+		margin-left: 0.5rem;
+	}
+`;
+
+// TagBox에서 사용하는 버튼과 일치하는 높이로 설정한 후 서로 간의 여백 지정
+const StyleButton = styled(Button)`
+	height: 2.125rem;
+	& + & {
+		margin-left: 0.5rem;
+	}
+`;
+
+const WriteActionButtons = ({ onCancel, onPublish }) => {
+	return (
+		<WriteActionButtonsWrapper>
+			<StyleButton cyan onClick={onPublish}>
+				포스트 등록
+			</StyleButton>
+			<StyleButton onClick={onCancel}>취소</StyleButton>
+		</WriteActionButtonsWrapper>
+	);
+};
+
+export default WriteActionButtons;
